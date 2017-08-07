0563981bdb26fb5d824e10bb63b3a1d82b9ab0fe
valeriadelrio <valeriadlrio@gmail.com>
2017-08-07 19:57:07 -0300

    Buscador andando

--- SPA/src/app/services/heroes.service.ts
+++ SPA/src/app/services/heroes.service.ts
@@ -66,6 +66,18 @@ export class HeroesService {
   public getHeroe(id: number):Heroe{
     return this.heroes[id];
   }
+
+  buscarHeroes(termino: string): Heroe[]{
+    let heroesArr: Heroe[]=[];
+    termino = termino.toLowerCase();
+    for(let heroe of this.heroes){
+      let nombre = heroe.nombre.toLowerCase();
+      if (nombre.indexOf( termino ) >= 0){
+        heroesArr.push(heroe);
+      }
+    }
+    return heroesArr;
+  }
 }
 
 export interface Heroe{
